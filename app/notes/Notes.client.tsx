a1049420534c9b03862bf81240301dea48f2f5cd
hellvita <seredaolga03@gmail.com>
2025-12-08 19:14:57 +0200

    add the NoteList component to the Notes page

--- app/notes/Notes.client.tsx
+++ app/notes/Notes.client.tsx
@@ -1,5 +1,18 @@
 "use client";
+import { fetchNotes } from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
+import NoteList from "@/components/NoteList/NoteList";
+import css from "./Notes.module.css";
 
-export default function NotesCLient() {
-  return <></>;
+export default function NotesClient() {
+  const { data } = useQuery({
+    queryKey: ["notes"],
+    queryFn: () => fetchNotes({}),
+  });
+
+  return (
+    <div className={css.notes_page}>
+      {data && data.notes.length > 0 && <NoteList notes={data.notes} />}
+    </div>
+  );
 }
--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,3 +1,15 @@
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from "@tanstack/react-query";
+import NotesClient from "./Notes.client";
+
 export default function Notes() {
-  return <></>;
+  const queryClient = new QueryClient();
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotesClient />
+    </HydrationBoundary>
+  );
 }
