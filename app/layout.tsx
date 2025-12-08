077619fb565d4521026d37f37661ac03d1179169
hellvita <seredaolga03@gmail.com>
2025-12-08 17:22:18 +0200

    add the Header content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
+import Header from "@/components/Header/Header";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -27,6 +28,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <TanStackProvider>
+          <Header />
+
           <main>{children}</main>
         </TanStackProvider>
       </body>
