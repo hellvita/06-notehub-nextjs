5466483601244b734ceb87b48e7d7405541da3a4
hellvita <seredaolga03@gmail.com>
2025-12-09 15:34:43 +0200

    add the button to create new note

--- app/notes/Notes.client.tsx
+++ app/notes/Notes.client.tsx
@@ -1,18 +1,36 @@
 "use client";
 import { fetchNotes } from "@/lib/api";
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import NoteList from "@/components/NoteList/NoteList";
+import Modal from "@/components/Modal/Modal";
+import NoteForm from "@/components/NoteForm/NoteForm";
 import css from "./Notes.module.css";
 
 export default function NotesClient() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const { data } = useQuery({
     queryKey: ["notes"],
     queryFn: () => fetchNotes({}),
   });
 
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
   return (
     <div className={css.notes_page}>
+      <header className={css.toolbar}>
+        <button className={css.button} onClick={openModal}>
+          Create note +
+        </button>
+      </header>
       {data && data.notes.length > 0 && <NoteList notes={data.notes} />}
+      {isModalOpen && (
+        <Modal onClose={closeModal}>
+          <NoteForm onClose={closeModal} />
+        </Modal>
+      )}
     </div>
   );
 }
