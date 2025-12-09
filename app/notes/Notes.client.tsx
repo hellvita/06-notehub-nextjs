0d5d2e59938d6c4e511cb2f7f5b2438a78a47717
hellvita <seredaolga03@gmail.com>
2025-12-09 15:48:16 +0200

    add the SearchBox component to the Notes page

--- app/notes/Notes.client.tsx
+++ app/notes/Notes.client.tsx
@@ -1,24 +1,37 @@
 "use client";
 import { fetchNotes } from "@/lib/api";
 import { useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
 import { useQuery, keepPreviousData } from "@tanstack/react-query";
-import NoteList from "@/components/NoteList/NoteList";
+import SearchBox from "@/components/SearchBox/SearchBox";
 import Pagination from "@/components/Pagination/Pagination";
 import Modal from "@/components/Modal/Modal";
 import NoteForm from "@/components/NoteForm/NoteForm";
+import NoResultMessage from "@/components/NoResultMessage/NoResultMessage";
+import NoteList from "@/components/NoteList/NoteList";
 import css from "./Notes.module.css";
 
 export default function NotesClient() {
   const [currentPage, setCurrentPage] = useState(1);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
 
   const { data, isSuccess } = useQuery({
-    queryKey: ["notes", currentPage],
-    queryFn: () => fetchNotes({ page: currentPage }),
+    queryKey: ["notes", currentPage, searchQuery],
+    queryFn: () =>
+      fetchNotes({
+        page: currentPage,
+        search: searchQuery !== "" ? searchQuery : undefined,
+      }),
     placeholderData: keepPreviousData,
   });
 
   const totalPages = data?.totalPages ?? 0;
+
+  const handleSearch = useDebouncedCallback((value: string) => {
+    setSearchQuery(value);
+    setCurrentPage(1);
+  }, 1000);
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
@@ -26,6 +39,7 @@ export default function NotesClient() {
   return (
     <div className={css.notes_page}>
       <header className={css.toolbar}>
+        <SearchBox query={searchQuery} onSearch={handleSearch} />
         {isSuccess && totalPages > 1 && (
           <Pagination
             totalPages={totalPages}
@@ -37,6 +51,9 @@ export default function NotesClient() {
           Create note +
         </button>
       </header>
+      {isSuccess && data.notes.length === 0 && searchQuery !== "" && (
+        <NoResultMessage invalidQuery={searchQuery} />
+      )}
       {data && data.notes.length > 0 && <NoteList notes={data.notes} />}
       {isModalOpen && (
         <Modal onClose={closeModal}>
