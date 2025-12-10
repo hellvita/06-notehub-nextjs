ff2f41ae3b6e36669ff7b3e8ffd2487083387dfc
hellvita <seredaolga03@gmail.com>
2025-12-10 18:13:43 +0200

    stick the Footer component to the bottom of the page

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
         <TanStackProvider>
           <Header />
 
-          {children}
+          <div className="main">{children}</div>
 
           <Footer />
         </TanStackProvider>
